dc13c6b76ddca884c81720d1db94d8185eb4440d
DONGWON LEE <bugehddnjs@naver.com>
2022-11-14 17:10:49 +0900

    회사에서 커밋

--- src/main/java/hello/hellospring/Springconfig.java
+++ src/main/java/hello/hellospring/Springconfig.java
@@ -1,36 +1,49 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
-public class Springconfig  {
+public class Springconfig {
 
-    private DataSource dataSource;
+//    private DataSource dataSource
+/*    private EntityManager em;
 
     @Autowired
-    public Springconfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public Springconfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Autowired*/
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public Springconfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+/*    @Bean
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
+    }*/
+
+    //@Bean
+    //public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
 
-    }
+   // }
 
 }
